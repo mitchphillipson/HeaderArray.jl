d936433f2fb5e943ac79bc086cc2a673e7bfd322
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-10-02 11:10:09 -0500

    Initial Upload

---
+++ src/HeaderArrayFile.jl
@@ -0,0 +1,28 @@
+module HeaderArrayFile
+
+    using DataFrames
+
+    import OrderedCollections: OrderedDict
+
+    include("structs.jl")
+
+    export HarHeader, name, HarMetadata, datatype, storage_type, description, dimension_sizes, 
+        HarRecord, HarFile
+
+    include("parameter.jl")
+
+    export HarParameter
+
+    include("sets.jl")
+
+    export HarSet
+
+    include("default.jl")
+
+    export HarDefaultData, add_data!
+
+    include("read.jl")
+
+    export read_chunk
+
+end # module HeaderArrayFile
